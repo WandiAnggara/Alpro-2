ca0cf99eaca68f3afd4938f0d2214c63e4673d51
WandiAnggara <61929971+WandiAnggara@users.noreply.github.com>
2020-03-08 15:08:17 +0800

    Update and rename Semester2.cpp to no1.cpp

---
+++ no1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int y,total,a;
+	int X[y];
+	cout<<"Jumlah Bilangan = ";cin>>y;
+	for(a = 0;a<x;a++){
+		cin>>X[a];
+	}
+	for(a = 0;a<x;a++){
+		total = total + X[a];
+	}
+	cout<<"Hasil Penjumlahan Bilangan = "<<total;
+}
